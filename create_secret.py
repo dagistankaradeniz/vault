ca49dbba1b71d4a1497b66064fdc8ac3d0da3ac6
Jake Furlong <jake.w.furlong@gmail.com>
2022-06-14 22:05:04 -0600

    updated python scripts

--- create_secret.py
+++ create_secret.py
@@ -2,12 +2,12 @@ from venv import create
 import hvac
 import json
 
-def write_secret():
+def create_secret():
 
     client = hvac.Client(url='http://localhost:8200')
-    create_response = client.secrets.kv.v2.create_or_update_secret(path='hello', secret=dict(walle="eve"))
+    create_response = client.secrets.kv.v2.create_or_update_secret(path='hello', secret=dict(foo="bar"))
 
     print(json.dumps(create_response, indent=4, sort_keys=True))
     
 
-write_secret()
+create_secret()
